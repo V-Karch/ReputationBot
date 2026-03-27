221587d263eb1f6cce83ca0101e273cf1c7b9caf
V-Karch <luna.karch05@gmail.com>
2026-03-27 10:31:46 -0400

    main.py: added members intent

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ with open("token.txt", "r") as f:
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 client = commands.Bot(command_prefix="!", intents=intents)
 
