598e2582b5380d3aae55e5cb900769acce701e68
V-Karch <luna.karch05@gmail.com>
2026-05-23 19:50:39 -0400

    autoreply: fixed minor errors with the formatting

--- cogs/autoreply.py
+++ cogs/autoreply.py
@@ -89,25 +89,36 @@ Pokemon Let's Go: <#1070473752695021598>"""
 
     @commands.command(name="guidelines", aliases=["gl"])
     async def guidelines(self, context: commands.Context):
-
         if context.author.id != 923600698967461898:
             await context.reply("Nope!")
             return  # Get out early if not me
 
         with open("model/trade_guidelines.txt", "r") as f:
-            first_half_data = [map(str.strip, f.readlines())]
+            first_half_data = "\n".join([i.strip() for i in f.readlines()])
 
         with open("model/trade_guidelines_special.txt", "r") as f:
-            second_half_data = [map(str.strip, f.readlines())]
+            second_half_data = "\n".join([i.strip() for i in f.readlines()])
 
         first_half_embed = discord.Embed(
-            discord.Color.red, title="Trade Guidelines", description=first_half_data
+            color=discord.Color.red(),
+            title="Trade Guidelines",
+            description=first_half_data,
         )
         second_half_embed = discord.Embed(
-            discord.Color.red, title="Special Cases", description=second_half_data
+            color=discord.Color.red(),
+            title="Special Cases",
+            description=second_half_data,
         )
 
-        await context.channel.send(embeds=[first_half_embed, second_half_embed])
+        accredation = discord.Embed(
+            color=discord.Color.green(),
+            title="Courtesy Of",
+            description="This guide was brought to you by <@558440155397095455>",
+        )
+
+        await context.channel.send(
+            embeds=[first_half_embed, second_half_embed, accredation]
+        )
 
 
 async def setup(client: commands.Bot):
